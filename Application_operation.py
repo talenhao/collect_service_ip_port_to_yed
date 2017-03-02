cff1add2cd5302bbd5fceb6672dd3e5705e653c9
tianfei hao <talenhao@gmail.com>
2017-03-02 11:34:20 +0800

    收集信息=》存储 yum install -y python-netifaces MySQL-python
    2017-02-27
        2.6.6版本不支持subprocess.check_out,修改为subprocess.Popen(["ls", "-a"], stdout=subprocess.PIPE).communicate()[0]
    2017-02-28
        使用re重写shell命令部分
        重写数据导入方法
        优化端口扫描方式，节省2/3时间
    2017-03-01
        解决插入数据重复问题
        改写appcolect为类方法
        调整数据录入逻辑
        ip地址不同版本系统获取问题
    未解决：
        程序匹配更多的项目类型，不仅限于java类的tomcat

---
+++ Application_operation.py
@@ -0,0 +1,217 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+# ******************************************************
+# Author       : tianfei hao
+# Create Time  : 2017-02-25
+# Last modified:
+# Email        : talenhao@gmail.com
+# Description  : collect listen ip port and connect socket.
+# ******************************************************
+# 数据库操作
+
+import MySQLdb
+
+# import datetime,os,sys
+
+"""
+'alter table application change project_id project_id tinyint(4) DEFAULT NULL auto_increment primarykey;'
+"""
+
+
+class DbInitConnect(object):
+    # 初始化基本变量
+    def __init__(self):
+        self.host = '192.168.1.138'
+        self.port = 3306
+        self.password = 'yed'
+        self.username = 'yed'
+        self.db = 'yed_collect'
+    # 连接数据库
+    
+    def Connect(self):
+        self.DBcon=MySQLdb.connect(host=self.host,
+                               port = self.port,
+                               user = self.username,
+                               passwd = self.password,
+                               db = self.db)
+        return self.DBcon
+        # 返回指针
+    
+    def Cursor(self):
+        self.Connect()
+        self.resultCursor=self.DBcon.cursor()
+        return self.resultCursor
+    
+    def ShowDatabases(self):
+        self.Cursor()
+        sql_cmd='show create database yed_collect'
+        #print(self.DBcon,self.resultCursor)
+        self.resultCursor.execute(sql_cmd)
+        for self.row in self.resultCursor.fetchall():
+            print self.row
+
+
+class GroupOperation(DbInitConnect):
+    def __init__(self):
+        # 初始父类的生成器
+        # super(DbInitConnect, self).__init__()
+        DbInitConnect.__init__(self)
+        self.grouptable = "appgroup"
+        self.Cursor()
+        # print(self.host,self.DBcon,self.resultCursor,self.resultCursor.description)
+        
+    def showGroup(self):
+        sql_cmd= 'SELECT groupname,parent_group from %s' % self.grouptable
+        self.resultCursor.execute(sql_cmd)
+        for self.row in self.resultCursor.fetchall():
+            print(self.row)
+            
+    def addGroup(self,groupname,parent_groupname='NULL'):
+        self.groupname=groupname
+        self.parent_groupname=parent_groupname
+        sql_cmd='INSERT INTO %s (groupname, parent_group) VALUES ("%s", "%s")' % (self.grouptable, self.groupname, self.parent_groupname)
+        print(sql_cmd)
+        self.resultCursor.execute(sql_cmd)
+        self.DBcon.commit()
+        
+    def delGroup(self,groupname):
+        self.groupname = groupname
+        sql_cmd = 'DELETE FROM %s where groupname="%s";' % (self.grouptable, self.groupname)
+        print(sql_cmd)
+        self.resultCursor.execute(sql_cmd)
+        self.DBcon.commit()
+        
+    def modifyGroup(self,groupname,new_groupname):
+        self.groupname = groupname
+        self.newgroupname = new_groupname
+        sql_cmd = 'UPDATE %s SET groupname="%s" where groupname="%s";' %(self.grouptable, self.newgroupname, self.groupname)
+        print(sql_cmd)
+        self.resultCursor.execute(sql_cmd)
+        self.DBcon.commit()
+        
+    def finallyCloseConnect(self):
+        self.DBcon.close()
+
+
+class applicationOperation(GroupOperation):
+    def __init__(self):
+        # 初始父类的生成器
+        # super(DbInitConnect, self).__init__()
+        GroupOperation.__init__(self)
+        self.projecttable = "application"
+    def showApplication(self):
+        sql_cmd= 'SELECT projectname,groupname from %s,%s where %s.groupid=%s.group_id' % (self.grouptable,self.projecttable,self.grouptable,self.projecttable)
+        self.resultCursor.execute(sql_cmd)
+        for self.row in self.resultCursor.fetchall():
+            print(self.row)
+
+    def addApplication(self,projectname,groupname):
+        self.groupname=groupname
+        self.projectname=projectname
+        if self.groupname is '':
+            self.groupname='in'
+        sql_cmd='SELECT groupid from %s where groupname="%s"' % (self.grouptable, self.groupname)
+        print(sql_cmd)
+        self.resultCursor.execute(sql_cmd)
+        self.groupid = self.resultCursor.fetchone()[0]
+        print('groupid is :',self.groupid)
+        sql_cmd='INSERT INTO %s (projectname, group_id) VALUES ("%s", %s)' % (self.projecttable, self.projectname, self.groupid)
+        # sql_cmd='INSERT INTO %s (projectname) VALUE ("%s") ' % (self.projecttable, self.projectname)
+        print(sql_cmd)
+        self.resultCursor.execute(sql_cmd)
+        self.DBcon.commit()
+
+    def delApplication(self, projectname):
+        self.projectname = projectname
+        sql_cmd = 'DELETE FROM %s where projectname="%s";' % (self.projecttable, self.projectname)
+        print(sql_cmd)
+        self.resultCursor.execute(sql_cmd)
+        self.DBcon.commit()
+
+    def modifyApplication(self,projectname,new_projectname):
+        self.projectname = projectname
+        self.newprojectname = new_projectname
+        sql_cmd = 'UPDATE %s SET projectname="%s" where projectname="%s";' %(self.projecttable, self.newprojectname, self.projectname)
+        print(sql_cmd)
+        self.resultCursor.execute(sql_cmd)
+        self.DBcon.commit()
+
+    def finallyCloseConnect(self):
+        self.DBcon.close()
+
+
+class Tui(object):
+    def __init__(self):
+        self.itemlist=['group','project']
+        self.operationlist=["show",'add','del','modify']
+
+    def headerline(self,item):
+        self.item=item
+        self.startheader = '>'*40+"In "+self.item+" choice: "+'<'*40
+        print(self.startheader)
+        
+    def endline(self):
+        self.endheader   = '='*40+'End'+'='*40
+        print(self.endheader)
+        
+    def listitem(self,list):
+        self.list=list
+        print(zip(range(len(self.list)),self.list))
+        
+if __name__ == "__main__":
+    Tuimenu=Tui()
+    Groupitem = GroupOperation()
+    Appitem   = applicationOperation()
+    flag = True
+    Tuimenu.headerline('start')
+    while flag:
+        Tuimenu.listitem(Tuimenu.itemlist)
+        try:
+            '''后期考虑添加退出'''
+            choiceitem = int(raw_input('请输入项目编号：\n'))
+        except ValueError:
+            print('Input a Num!')
+            continue
+        if int(choiceitem) not in range(len(Tuimenu.itemlist)):
+            print(range(len(Tuimenu.itemlist)))
+            print('Error num')
+            continue
+        Tuimenu.listitem(Tuimenu.operationlist)
+        choiceitem2 = int(raw_input('请输入项目编号：\n'))
+        if int(choiceitem2) not in range(len(Tuimenu.operationlist)):
+            print(range(len(Tuimenu.operationlist)))
+            print('Error num')
+            continue
+        print('process')
+        if choiceitem == 0 and choiceitem2 == 0 :
+            Groupitem.showGroup()
+        elif choiceitem == 0 and choiceitem2 == 1:
+            groupinput=raw_input("请输入group名称：\n")
+            parentgroupinput=raw_input("请输入父group名称，留空不添加父组：\n")
+            Groupitem.addGroup(groupinput,parentgroupinput)
+        elif choiceitem == 0 and choiceitem2 ==2:
+            Groupitem.showGroup()
+            groupinput = raw_input("请输入要删除的项目名：\n").strip()
+            Groupitem.delGroup(groupinput)
+        elif choiceitem == 0 and choiceitem2 == 3:
+            Groupitem.showGroup()
+            groupinput = raw_input("老项目名：\n").strip()
+            newgroupinput = raw_input("new项目名").strip()
+            Groupitem.modifyGroup(groupinput,newgroupinput)
+        elif choiceitem == 1 and choiceitem2 == 0 :
+            Appitem.showApplication()
+        elif choiceitem == 1 and choiceitem2 == 1:
+            appinput=raw_input("请输入app名称：\n")
+            parentappinput=raw_input("请输入父app名称，留空不添加组：\n")
+            Appitem.addApplication(appinput,parentappinput)
+        elif choiceitem == 1 and choiceitem2 ==2:
+            Appitem.showApplication()
+            appinput = raw_input("请输入要删除的项目名：\n").strip()
+            Appitem.delApplication(appinput)
+        elif choiceitem == 1 and choiceitem2 == 3:
+            Appitem.showApplication()
+            appinput = raw_input("老项目名：\n").strip()
+            newappinput = raw_input("new项目名").strip()
+            Appitem.modifyApplication(appinput,newappinput)
+    Tuimenu.endline()
+        # for key,value in Tuimenu.listitem():
---
+++ yed_collect_agent.py
@@ -0,0 +1,266 @@
+#!/bin/env python
+# coding=utf-8
+
+# -*- coding:utf-8 -*-
+
+# ******************************************************
+# Author       : tianfei hAppOp
+# Create Time  : 2017-02-25
+# Last modified:
+# Email        : talenhAppOp@gmail.com
+# Description  : collect listen ip port and connect socket.
+# ******************************************************
+
+"""
+收集信息=》存储
+yum install -y python-netifaces MySQL-python
+
+2017-02-27
+    2.6.6版本不支持subprocess.check_out,修改为subprocess.Popen(["ls", "-a"], stdout=subprocess.PIPE).communicate()[0]
+2017-02-28
+    使用re重写shell命令部分
+    重写数据导入方法
+    优化端口扫描方式，节省2/3时间
+2017-03-01
+    解决插入数据重复问题
+    改写appcolect为类方法
+    调整数据录入逻辑
+    ip地址不同版本系统获取问题
+未解决：
+    程序匹配更多的项目类型，不仅限于java类的tomcat
+
+"""
+
+
+from Application_operation import applicationOperation as AppOp
+import subprocess
+import shlex
+import sys
+import os
+import re
+import netifaces
+
+
+if sys.version_info < (2, 7):
+    # raise RuntimeError('At least Python 3.4 is required')
+    print('友情提示：当前系统版本低于2.7，建议升级python版本。')
+
+
+class AppListen(AppOp):
+    """
+    收集监听IP及port信息
+    收集连接池信息
+    """
+    
+    def __init__(self):
+        AppOp.__init__(self)
+
+    def project_list(self):
+        sql_cmd = "SELECT projectname from %s" % self.projecttable # self.projecttable来自AppOp
+        # print(sql_cmd)
+        self.resultCursor.execute(sql_cmd)
+        self.projectsnamelist = []
+        for self.row in self.resultCursor.fetchall():
+            self.projectsnamelist.append(self.row[0])
+        return self.projectsnamelist
+    
+    def collect_pid_list(self, project):
+        """
+        查找指定程序的PID
+        :param project:
+        :return:
+        """
+        self.pid_lists = []
+        self.project = project
+        print("查找程序%s的pid" % self.project)
+        try:
+            # ps aux |grep -E '[D]catalina.home=/data/wire/tomcat'|awk '{print $2}'
+            # 1.内容
+            ps_aux_cmd = 'ps aux'
+            ps_aux_result = subprocess.Popen(shlex.split(ps_aux_cmd), stdout=subprocess.PIPE)
+            ps_aux_result_text = ps_aux_result.communicate()[0]  # .decode('utf-8')
+            # 2.pattern&compile
+            ps_aux_pattern_tomcat = 'Dcatalina.home=/[-\w]+/%s/tomcat' % self.project
+            ps_aux_compile = re.compile(ps_aux_pattern_tomcat)
+            # 3.match object
+            for ps_aux_result_line in ps_aux_result_text.splitlines():
+                ps_aux_re_find = ps_aux_compile.findall(ps_aux_result_line)
+                if ps_aux_re_find:
+                    print("匹配到： %s " % ps_aux_re_find)
+                    self.pid = int(ps_aux_result_line.split()[1])
+                    print('%s has a pid number %s ...' % (self.project, self.pid))
+                    self.pid_lists.append(self.pid)
+        except subprocess.CalledProcessError:
+            print('%s is not in this host!' % self.project)
+        print("程序%s的pid结果为：%s" % (self.project, self.pid_lists))
+        return self.pid_lists
+    
+    def get_localhost_ip_list(self):
+        """
+        获取本机所有IP信息
+        """
+        self.card_ip_list = []
+        for interface_card in netifaces.interfaces():
+            try:
+                card_ip_address = netifaces.ifaddresses(interface_card)[netifaces.AF_INET][0]['addr']
+            except KeyError:
+                print("%s is not have ip" % interface_card)
+            else:
+                self.card_ip_list.append(card_ip_address)
+        #self.card_ip_list_all = self.card_ip_list.remove('127.0.0.1')
+        return self.card_ip_list
+
+    def listenports(self, pids):
+        '''
+        接收列表[pids]
+        :param pids:
+        :return:
+        '''
+        self.pidlist = pids
+        if not self.pidlist:
+            return None
+        self.portlists = []
+        # 1.内容
+        # sscmd = "ss -lntp -4 |grep %s |awk -F: '{print $2}'|awk '{print $1}'" % ipid
+        sscmd = 'ss -l -n -p -t'
+        sscmd_result = subprocess.Popen(shlex.split(sscmd), stdout=subprocess.PIPE)
+        sscmd_result_text = sscmd_result.communicate()[0]  # .decode('utf-8')
+        # print("ss -l结果： %s" % sscmd_result_text)
+        # 2.pattern&compile
+        sscmd_pattern_pid = '|'.join(format(n) for n in self.pidlist)
+        print("pattern is :%s" % sscmd_pattern_pid)
+        sscmd_compile = re.compile(sscmd_pattern_pid)
+        # 3.match object
+        for sscmd_result_line in sscmd_result_text.splitlines():
+            sscmd_re_findpid = sscmd_compile.findall(sscmd_result_line)
+            if sscmd_re_findpid:
+                print(sscmd_result_line)
+                self.listenport = sscmd_result_line.split()[3].split(':')[-1].strip()
+                print(self.listenport)
+                self.portlists.append(self.listenport)
+        print("监听端口接收到的监听列表%s" % self.portlists)
+        return self.portlists
+
+    def connectpools(self, ports, pids):
+        """
+
+        :param ports:
+        :param pids:
+        :return: 连接池
+        """
+        ports = ports
+        pids = pids
+        self.poollists = []
+        print("处理连接池，接收参数端口：%s，进程号：%s" % (ports, pids))
+        self.sportline = ["sport neq :%s" % n for n in ports]
+        self.sportjoin = ' and '.join(self.sportline)
+        # 1.内容
+        # sscmd = ss -ntp -o state established \'( sport != :%s )\'|grep -E  %s |awk \'{print $4}\'|awk -F \':\' \'{print $(NF-1)\":\" $NF}\'' % (self.port,self.pid)
+        ssntpcmd = 'ss -ntp -o state established \\( %s \\)' % self.sportjoin
+        print("ssntpcmd is: %s " % ssntpcmd)
+        ssntpcmd_result = subprocess.Popen(shlex.split(ssntpcmd), stdout=subprocess.PIPE)
+        ssntpcmd_result_text = ssntpcmd_result.communicate()[0]  # .decode('utf-8')
+        # print("ssntpcmd_result_text is %s" % ssntpcmd_result_text)
+        # 2.pattern&compile
+        ssntpcmd_pattern_pid = ',%s,' % self.pid
+        ssntpcmd_compile = re.compile(ssntpcmd_pattern_pid)
+        # 3.match object
+        for ssntpcmd_result_line in ssntpcmd_result_text.splitlines():
+            ssntpcmd_re_findpid = ssntpcmd_compile.findall(ssntpcmd_result_line)
+            if ssntpcmd_re_findpid:
+                print("当前连接池匹配行：%s" % ssntpcmd_result_line)
+                print("当前pid匹配结果：%s" % ssntpcmd_re_findpid)
+                self.connect_to_ipportlist = ssntpcmd_result_line.split()[3].split(':')[-2:]
+                print("过滤出的连接池IP：port %s" % self.connect_to_ipportlist)
+                self.ipportmessage = ':'.join(self.connect_to_ipportlist)
+                self.poollists.append(self.ipportmessage)
+        print("处理连接池，列表：%s" % self.poollists)
+        return self.poollists
+
+    def import2db(self, table, ipportcolumn, projectcolumn, message):
+        """
+        MySQL [yed_collect]> alter table listentable add primary key (lipport,projectname);
+                MySQL [yed_collect]> show create table listentable\G;
+        *************************** 1. row ***************************
+               Table: listentable
+        Create Table: CREATE TABLE `listentable` (
+          `lipport` varchar(24) NOT NULL,
+          `projectname` varchar(20) NOT NULL,
+          PRIMARY KEY (`lipport`,`projectname`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+        1 row in set (0.07 sec)
+        MySQL [yed_collect]> alter table pooltable add primary key (conipport,projectname);
+                MySQL [yed_collect]> show create table pooltable\G;
+        *************************** 1. row ***************************
+               Table: pooltable
+        Create Table: CREATE TABLE `pooltable` (
+          `conipport` varchar(24) NOT NULL,
+          `projectname` varchar(20) NOT NULL,
+          PRIMARY KEY (`conipport`,`projectname`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+        1 row in set (0.12 sec)
+
+        :param table:
+        :param ipportcolumn:
+        :param projectcolumn:
+        :param message:
+        :return:
+        """
+        if len(message) > 0:
+            self.message = message
+            self.ipportcolumn = ipportcolumn
+            self.table = table
+            self.projectcolumn = projectcolumn
+            sql_cmd = "INSERT ignore INTO %s (%s,%s) VALUES %s" % (self.table, self.ipportcolumn, self.projectcolumn, ','.join(self.message))
+            print(sql_cmd)
+            self.resultCursor.execute(sql_cmd)
+            self.DBcon.commit()
+        else:
+            print("%s is not have socket." % self.project)
+
+
+def app_l_collect():
+    # 取出应用名称
+    app_listen_instance = AppListen()
+    app_listen_con_db_project_list = app_listen_instance.project_list()
+    listentable = "listentable"
+    listen_ipport_column = 'lipport'
+    connectpooltable = "pooltable"
+    connectpool_ipport_column = 'conipport'
+    projectcolumn = 'projectname'
+
+    for project_item in app_listen_con_db_project_list:  # project name list
+        todb_ipport_project = []
+        todb_conipport_project = []
+        # rows, columns = os.popen('stty size', 'r').read().split()
+        # print("=" * int(columns) + "\n 1.process project : %s \n" % project_item)
+        print("=" * 80 + "\n 1.process project : %s \n" % project_item)
+        # print(type(project_item))
+        from_db_pid_list = app_listen_instance.collect_pid_list(project_item)  # =>pid列表
+        print("2.查询到的pid列表：%s" % from_db_pid_list)
+        from_db_ports = app_listen_instance.listenports(from_db_pid_list)  # =>port列表
+        if not from_db_ports:
+            print("%s 没有在此服务器上运行..." % project_item)
+            continue
+        # 生成连接池表
+        collect_con_ipport_list = app_listen_instance.connectpools(from_db_ports, from_db_pid_list)
+        # 生成监听信息
+        for port in from_db_ports:
+            # 导入监听表
+            # 监听信息需要提前合成
+            for ip in app_listen_instance.get_localhost_ip_list():
+                ipport = str(ip)+':'+str(port)
+                linfo = "('"+ipport+"','"+project_item+"')"
+                todb_ipport_project.append(linfo)
+        # 生成连接池信息
+        for con in collect_con_ipport_list:
+            cinfo = "('"+con+"','"+project_item+"')"
+            print("cinfo is :%s" % cinfo)
+            todb_conipport_project.append(cinfo)
+        # print("导入ip列表%s" % todb_ipport_project)
+        app_listen_instance.import2db(listentable, listen_ipport_column, projectcolumn, todb_ipport_project)
+        # print("导入connect列表%s" % todb_conipport_project)
+        app_listen_instance.import2db(connectpooltable, connectpool_ipport_column, projectcolumn, todb_conipport_project)
+
+if __name__ == "__main__":
+    app_l_collect()
