bab20fcc79543dd4458f39f88142ee4578a5b19e
tianfei hao <talenhao@gmail.com>
2017-04-14 15:59:26 +0800

    添加日志功能;修整代码格式及规范;添加退出功能。

--- Application_operation.py
+++ Application_operation.py
@@ -14,9 +14,20 @@
 """
 
 import MySQLdb
+import datetime
+import sys
+import logging
+import collect_log
+
+
+LogPath = '/tmp/%s.log.%s' % (sys.argv[0], datetime.datetime.now().strftime('%Y-%m-%d,%H.%M'))
+c_logger = collect_log.GetLogger(LogPath, __name__, logging.DEBUG).get_l()
 
 
 class DbInitConnect(object):
+    """
+    数据库初始化及连接，游标
+    """
     # 初始化基本变量
     def __init__(self):
         self.host = '192.168.1.138'
@@ -24,201 +35,222 @@ class DbInitConnect(object):
         self.password = 'yed'
         self.username = 'yed'
         self.db = 'yed_collect'
-    # 连接数据库
-    
-    def Connect(self):
-        self.DBcon = MySQLdb.connect(host=self.host,
-                               port = self.port,
-                               user = self.username,
-                               passwd = self.password,
-                               db = self.db)
-        return self.DBcon
-        # 返回指针
-    
-    def Cursor(self):
-        self.Connect()
-        self.resultCursor=self.DBcon.cursor()
-        return self.resultCursor
-    
-    def ShowDatabases(self):
-        self.Cursor()
-        sql_cmd='show create database yed_collect'
-        # print(self.DBcon,self.resultCursor)
-        self.resultCursor.execute(sql_cmd)
-        for self.row in self.resultCursor.fetchall():
-            print self.row
 
-    def finally_close_connect(self):
-        self.DBcon.close()
+    # 连接数据库
+    def db_connect(self):
+        self.connect = MySQLdb.connect(host=self.host,
+                                     port=self.port,
+                                     user=self.username,
+                                     passwd=self.password,
+                                     db=self.db)
+        # 返回指针
+        return self.connect
+
+    # 游标
+    def db_cursor(self):
+        self.cursor = self.db_connect().cursor()
+        return self.cursor
+
+    def finally_close_all(self):
+        """
+        关闭游标，关闭连接。
+        :return: 
+        """
+        self.cursor.close()
+        self.connect.close()
+
+    def show_databases(self):
+        cursor = self.db_cursor()
+        sql_cmd = 'show create database yed_collect'
+        try:
+            cursor.execute(sql_cmd)
+        except:
+            c_logger.info('数据库连接有问题。')
+        finally:
+            for row in cursor.fetchall():
+                c_logger.info(row)
+
 
 class GroupOperation(DbInitConnect):
     def __init__(self):
         # 初始父类的生成器
         # super(DbInitConnect, self).__init__()
         DbInitConnect.__init__(self)
+        DbInitConnect.db_cursor(self)
         self.group_table = "appgroup"
-        self.Cursor()
-        # print(self.host,self.DBcon,self.resultCursor,self.resultCursor.description)
-        
-    def ShowGroup(self):
+
+    def show_group(self):
         sql_cmd= 'SELECT groupname,parent_group from %s' % self.group_table
-        self.resultCursor.execute(sql_cmd)
-        for self.row in self.resultCursor.fetchall():
-            print(self.row)
+        self.cursor.execute(sql_cmd)
+        for row in self.cursor.fetchall():
+            print(row)
             
-    def addGroup(self,groupname,parent_groupname='NULL'):
-        self.groupname=groupname
-        self.parent_groupname=parent_groupname
-        sql_cmd = 'INSERT INTO %s (groupname, parent_group) VALUES ("%s", "%s")' % (self.group_table, self.groupname, self.parent_groupname)
-        print(sql_cmd)
-        self.resultCursor.execute(sql_cmd)
-        self.DBcon.commit()
+    def add_group(self, group_name, parent_group_name=''):
+        sql_cmd = 'INSERT IGNORE INTO %s (groupname, parent_group) VALUES ("%s", "%s")' % (
+            self.group_table,
+            group_name,
+            parent_group_name)
+        c_logger.info(sql_cmd)
+        self.cursor.execute(sql_cmd)
         
-    def delGroup(self,groupname):
-        self.groupname = groupname
-        sql_cmd = 'DELETE FROM %s where groupname="%s";' % (self.group_table, self.groupname)
-        print(sql_cmd)
-        self.resultCursor.execute(sql_cmd)
-        self.DBcon.commit()
-        
-    def modifyGroup(self,groupname,new_groupname):
-        self.groupname = groupname
-        self.newgroupname = new_groupname
-        sql_cmd = 'UPDATE %s SET groupname = "%s" where groupname = "%s";' %(self.group_table, self.newgroupname, self.groupname)
-        print(sql_cmd)
-        self.resultCursor.execute(sql_cmd)
-        self.DBcon.commit()
-        
-    def finallyCloseConnect(self):
-        self.DBcon.close()
+    def del_group(self, group_name):
+        sql_cmd = 'DELETE FROM %s where groupname="%s";' % (
+            self.group_table,
+            group_name)
+        c_logger.info(sql_cmd)
+        try:
+            self.cursor.execute(sql_cmd)
+        except:
+            c_logger.info("删除失败。")
+
+    def modify_group(self, old_name, new_name):
+        sql_cmd = 'UPDATE %s SET groupname = "%s" where groupname = "%s";' % (
+            self.group_table,
+            old_name,
+            new_name)
+        c_logger.info(sql_cmd)
+        self.cursor.execute(sql_cmd)
 
 
-class applicationOperation(GroupOperation):
+class ApplicationOperation(GroupOperation):
     def __init__(self):
         # 初始父类的生成器
         # super(DbInitConnect, self).__init__()
         GroupOperation.__init__(self)
-        self.projecttable = "application"
-    def showApplication(self):
-        sql_cmd= 'SELECT projectname,groupname from %s,%s where %s.groupid=%s.group_id' % (self.group_table,self.projecttable,self.group_table,self.projecttable)
-        self.resultCursor.execute(sql_cmd)
-        for self.row in self.resultCursor.fetchall():
-            print(self.row)
+        self.project_table = "application"
 
-    def addApplication(self,projectname,groupname):
-        self.groupname=groupname
-        self.projectname=projectname
-        if not self.groupname:
-            self.groupname='in'
-        sql_cmd='SELECT groupid from %s where groupname="%s"' % (self.group_table, self.groupname)
-        print(sql_cmd)
-        self.resultCursor.execute(sql_cmd)
-        self.groupid = self.resultCursor.fetchone()[0]
-        print('groupid is :',self.groupid)
-        sql_cmd='INSERT INTO %s (projectname, group_id) VALUES ("%s", %s)' % (self.projecttable, self.projectname, self.groupid)
-        # sql_cmd='INSERT INTO %s (projectname) VALUE ("%s") ' % (self.projecttable, self.projectname)
-        print(sql_cmd)
+    def show_application(self):
+        sql_cmd = 'SELECT projectname, groupname from %s a, %s b where a.groupid = b.group_id' % (
+            self.group_table,
+            self.project_table)
+        self.cursor.execute(sql_cmd)
+        for row in self.cursor.fetchall():
+            print(row)
+
+    def add_application(self, app_name, group_name='in'):
+        sql_cmd='SELECT groupid from %s where groupname = "%s"' % (
+            self.group_table,
+            group_name)
+        c_logger.info(sql_cmd)
+        self.cursor.execute(sql_cmd)
+        group_id = self.cursor.fetchone()[0]
+        c_logger.info('group id is :', group_id)
+        sql_cmd = 'INSERT IGNORE INTO %s (projectname, group_id) VALUES ("%s", %s)' % (
+            self.project_table,
+            app_name,
+            group_name)
+        c_logger.info(sql_cmd)
         try:
-            self.resultCursor.execute(sql_cmd)
+            self.cursor.execute(sql_cmd)
         # except _mysql_exceptions.IntegrityError as e:
         except:
                 # Another instance already created in this column
                 print("Duplicate!")
-                # print(e)
-        else:
-            self.DBcon.commit()
 
-    def delApplication(self, projectname):
-        self.projectname = projectname
-        sql_cmd = 'DELETE FROM %s where projectname="%s";' % (self.projecttable, self.projectname)
-        print(sql_cmd)
-        self.resultCursor.execute(sql_cmd)
-        self.DBcon.commit()
+    def del_application(self, app_name):
+        sql_cmd = 'DELETE FROM %s where projectname = "%s";' % (self.project_table, app_name)
+        c_logger.info(sql_cmd)
+        self.cursor.execute(sql_cmd)
 
-    def modifyApplication(self,projectname,new_projectname):
-        self.projectname = projectname
-        self.newprojectname = new_projectname
-        sql_cmd = 'UPDATE %s SET projectname="%s" where projectname="%s";' %(self.projecttable, self.newprojectname, self.projectname)
-        print(sql_cmd)
-        self.resultCursor.execute(sql_cmd)
-        self.DBcon.commit()
-
-    def finallyCloseConnect(self):
-        self.DBcon.close()
+    def modify_application(self, old_app, new_app):
+        sql_cmd = 'UPDATE %s SET projectname = "%s" where projectname="%s";' %(self.project_table, new_app, old_app)
+        c_logger.info(sql_cmd)
+        self.cursor.execute(sql_cmd)
 
 
 class Tui(object):
     def __init__(self):
-        self.itemlist=['group','project']
-        self.operationlist=["show",'add','del','modify']
+        self.item_list = ['group', 'project']
+        self.operation_list = ["show", 'add', 'del', 'modify']
 
-    def headerline(self,item):
-        self.item=item
-        self.startheader = '>'*40+"In "+self.item+" choice: "+'<'*40
-        print(self.startheader)
+    @staticmethod
+    def header_line(item):
+        start_header = '>' * 40 + "In " + item + " choice: " + '<' * 40
+        print(start_header)
+
+    @staticmethod
+    def end_line():
+        end_header = '=' * 40 + 'End' + '=' * 40
+        print(end_header)
+    
+    @staticmethod
+    def list_item(list_name):
+        print(zip(range(len(list_name)), list_name))
         
-    def endline(self):
-        self.endheader   = '='*40+'End'+'='*40
-        print(self.endheader)
-        
-    def listitem(self,list):
-        self.list=list
-        print(zip(range(len(self.list)),self.list))
-        
+
 if __name__ == "__main__":
-    Tuimenu=Tui()
-    Groupitem = GroupOperation()
-    Appitem   = applicationOperation()
+    Tui_menu = Tui()
+    group_item = GroupOperation()
+    app_item = ApplicationOperation()
     flag = True
-    Tuimenu.headerline('start')
+    Tui_menu.header_line('start')
     while flag:
-        Tuimenu.listitem(Tuimenu.itemlist)
+        Tui_menu.list_item(Tui_menu.item_list)
         try:
-            '''后期考虑添加退出'''
-            choiceitem = int(raw_input('请输入项目编号：\n'))
+            choice_item_input = raw_input('请输入项目编号：\n')
+            choice_item = int(choice_item_input)
         except ValueError:
-            print('Input a Num!')
-            continue
-        if int(choiceitem) not in range(len(Tuimenu.itemlist)):
-            print(range(len(Tuimenu.itemlist)))
+            if str(choice_item_input) in ['q', 'quit']:
+                break
+            else:
+                print('Input a Num!')
+                continue
+        if int(choice_item) not in range(len(Tui_menu.item_list)):
+            print(range(len(Tui_menu.item_list)))
             print('Error num')
             continue
-        Tuimenu.listitem(Tuimenu.operationlist)
-        choiceitem2 = int(raw_input('请输入项目编号：\n'))
-        if int(choiceitem2) not in range(len(Tuimenu.operationlist)):
-            print(range(len(Tuimenu.operationlist)))
+        Tui_menu.list_item(Tui_menu.operation_list)
+        try:
+            choice_item_op_input = int(raw_input('请输入项目编号：\n'))
+            Tui_menu.header_line('RESULT')
+            choice_item_op = int(choice_item_op_input)
+        except ValueError:
+            if str(choice_item_op) in ["q", "quit"]:
+                break
+            else:
+                print('Input a Num!')
+                continue
+        if int(choice_item_op) not in range(len(Tui_menu.operation_list)):
+            print(range(len(Tui_menu.operation_list)))
             print('Error num')
             continue
-        print('process')
-        if choiceitem == 0 and choiceitem2 == 0 :
-            Groupitem.ShowGroup()
-        elif choiceitem == 0 and choiceitem2 == 1:
-            groupinput=raw_input("请输入group名称：\n")
-            parentgroupinput=raw_input("请输入父group名称，留空不添加父组：\n")
-            Groupitem.addGroup(groupinput,parentgroupinput)
-        elif choiceitem == 0 and choiceitem2 ==2:
-            Groupitem.ShowGroup()
-            groupinput = raw_input("请输入要删除的项目名：\n").strip()
-            Groupitem.delGroup(groupinput)
-        elif choiceitem == 0 and choiceitem2 == 3:
-            Groupitem.ShowGroup()
-            groupinput = raw_input("老项目名：\n").strip()
-            newgroupinput = raw_input("new项目名").strip()
-            Groupitem.modifyGroup(groupinput,newgroupinput)
-        elif choiceitem == 1 and choiceitem2 == 0 :
-            Appitem.showApplication()
-        elif choiceitem == 1 and choiceitem2 == 1:
-            appinput=raw_input("请输入app名称：\n")
-            parentappinput=raw_input("请输入父app名称，留空不添加组：\n")
-            Appitem.addApplication(appinput,parentappinput)
-        elif choiceitem == 1 and choiceitem2 ==2:
-            Appitem.showApplication()
-            appinput = raw_input("请输入要删除的项目名：\n").strip()
-            Appitem.delApplication(appinput)
-        elif choiceitem == 1 and choiceitem2 == 3:
-            Appitem.showApplication()
-            appinput = raw_input("老项目名：\n").strip()
-            newappinput = raw_input("new项目名").strip()
-            Appitem.modifyApplication(appinput,newappinput)
-    Tuimenu.endline()
-        # for key,value in Tuimenu.listitem():
+        c_logger.debug('process')
+        if choice_item == 0 and choice_item_op == 0:
+            group_item.show_group()
+            Tui_menu.end_line()
+        elif choice_item == 0 and choice_item_op == 1:
+            group_input = raw_input("请输入group名称：\n")
+            parent_group_input = raw_input("请输入父group名称，留空不添加父组：\n")
+            group_item.add_group(group_input, parent_group_input)
+            Tui_menu.end_line()
+        elif choice_item == 0 and choice_item_op == 2:
+            group_item.show_group()
+            group_input = raw_input("请输入要删除的项目名：\n").strip()
+            group_item.del_group(group_input)
+            Tui_menu.end_line()
+        elif choice_item == 0 and choice_item_op == 3:
+            group_item.show_group()
+            group_input = raw_input("老项目名：\n").strip()
+            new_group_input = raw_input("new项目名").strip()
+            group_item.modify_group(group_input, new_group_input)
+            Tui_menu.end_line()
+        elif choice_item == 1 and choice_item_op == 0:
+            app_item.show_application()
+            Tui_menu.end_line()
+        elif choice_item == 1 and choice_item_op == 1:
+            app_input = raw_input("请输入app名称：\n")
+            parent_app_input = raw_input("请输入父app名称，留空不添加组：\n")
+            app_item.add_application(app_input, parent_app_input)
+            Tui_menu.end_line()
+        elif choice_item == 1 and choice_item_op == 2:
+            app_item.show_application()
+            app_input = raw_input("请输入要删除的项目名：\n").strip()
+            app_item.del_application(app_input)
+            Tui_menu.end_line()
+        elif choice_item == 1 and choice_item_op == 3:
+            app_item.show_application()
+            app_input = raw_input("老项目名：\n").strip()
+            new_app_input = raw_input("new项目名").strip()
+            app_item.modify_application(app_input,new_app_input)
+            Tui_menu.end_line()
+    app_item.finally_close_all()
